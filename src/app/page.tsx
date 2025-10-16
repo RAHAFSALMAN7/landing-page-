2da6a0f473ca0fc486e809ca0006ba956723fee7
Rahafsalman <144127007+RAHAFSALMAN7@users.noreply.github.com>
2025-10-16 15:29:51 +0300

    تحسين تصميم القصة + الفيديو وجعلها متجاوبة على الموبايل

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 "use client";
 import React from "react";
 import Image from "next/image";
